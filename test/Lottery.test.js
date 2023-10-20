84078c68f763656056e1eeabb9a5d93dd528d3a2
Chavidu <chavidukalpana40@gmail.com>
2023-10-21 00:38:39 +0530

    fix: code format

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -2,7 +2,6 @@ const ganache = require('ganache');
 const { Web3 } = require('web3');
 const assert = require('assert');
 const web3 = new Web3(ganache.provider());
-
 const {interface, bytecode} = require('../compile');
 
 let lottery;
